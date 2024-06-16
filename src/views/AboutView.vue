0df8d530d5d697e80f19c84a2e26c0c967cc5c97
Natalia <nataliasuert@gmail.com>
2024-06-16 22:42:28 +0200

    AboutUs View STYLES

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,42 +1,62 @@
+<script setup>
+import WelcomeItem from '../components/WelcomeItem.vue'
+import DocumentationIcon from '../components/icons/IconDocumentation.vue'
+</script>
 <template>
-  <div class="about">
-    <div class="item">
-      <h1 class="green">Nuestra Misión</h1>
-      <p>Nuestra misión es ofrecer soluciones tecnológicas innovadoras y personalizadas que impulsen la eficiencia,
-        mejoren la productividad y desbloqueen nuevas oportunidades de crecimiento. Creemos en el poder de la tecnología
-        para transformar negocios y estamos comprometidos a guiarlo en cada paso de su viaje digital. </p>
-    </div>
-    <div class="item"><h1 class="green">Qué hacemos</h1></div>
-    <div class="wrapper">
-      <h2 class="green">Experiencia en Productos de Google</h2>
-      <p>Como expertos certificados en productos de Google, proporcionamos soluciones integrales para integrar y
-        optimizar
-        la suite de herramientas de Google en su empresa. Desde Google Workspace hasta Google Cloud, nos aseguramos de
-        que
-        aproveche todo el potencial de estas potentes plataformas.</p>
-    </div>
-    <div class="wrapper">
-    <h2 class="green">Transición Digital</h2>
-    <p>Navegar por el panorama digital puede ser un desafío. Nuestro equipo de profesionales experimentados se
-      especializa
-      en transiciones digitales sin contratiempos, asegurando que su negocio se adapte a nuevas tecnologías con mínima
-      interrupción. Le ayudamos a modernizar su infraestructura y procesos para mantenerse competitivo.</p>
-    </div>
-    <h2 class="green">Automatización de Procesos</h2>
-    <p>Maximice la eficiencia y reduzca errores automatizando sus procesos empresariales. En Gadana Solutions, diseñamos
-      y
-      ejecutamos soluciones de automatización personalizadas que optimizan sus operaciones, ahorrándole tiempo y
-      recursos
-      valiosos.</p>
-    <h2 class="green">Análisis de Datos</h2>
-    <p>En la era de la información, los datos son el activo más valioso. Ofrecemos servicios avanzados de análisis de
+  <main>
+    <WelcomeItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Nuestra Misión</template>
+      Nuestra misión es ofrecer soluciones tecnológicas innovadoras y personalizadas que impulsen la eficiencia,
+      mejoren la productividad y desbloqueen nuevas oportunidades de crecimiento. Creemos en el poder de la tecnología
+      para transformar negocios y estamos comprometidos a guiarlo en cada paso de su viaje digital.
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Experiencia en Productos de Google</template>
+      Como expertos certificados en productos de Google, proporcionamos soluciones integrales para integrar y
+      optimizar la suite de herramientas de Google en su empresa. Desde Google Workspace hasta Google Cloud,
+      nos aseguramos de que aproveche todo el potencial de estas potentes plataformas.
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Transición Digital</template>
+      Navegar por el panorama digital puede ser un desafío. Nuestro equipo de profesionales experimentados se
+      especializa en transiciones digitales sin contratiempos, asegurando que su negocio se adapte a nuevas tecnologías
+      con
+      mínima
+      interrupción. Le ayudamos a modernizar su infraestructura y procesos para mantenerse competitivo.
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Automatización de Procesos</template>
+      Maximice la eficiencia y reduzca errores automatizando sus procesos empresariales. En Gadana Solutions, diseñamos
+      y ejecutamos soluciones de automatización personalizadas que optimizan sus operaciones, ahorrándole tiempo y
+      recursos valiosos.
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Análisis de Datos</template>
+      En la era de la información, los datos son el activo más valioso. Ofrecemos servicios avanzados de análisis de
       datos
       que transforman sus datos en conocimientos accionables, impulsando decisiones estratégicas y mejorando el
-      rendimiento general de su negocio.</p>
-    <h3 class="green">En Gadana Solutions, estamos comprometidos con su éxito. Permítanos ser su guía en el camino hacia
-      la innovación
-      tecnológica y el crecimiento sostenible.</h3>
-  </div>
+      rendimiento general de su negocio.
+    </WelcomeItem>
+
+
+
+
+  </main>
 </template>
 
 <style>
