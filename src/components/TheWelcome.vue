335154361a3514daacfc6d0be55cfb0e22c06370
Natalia <nataliasuert@gmail.com>
2024-06-16 19:56:47 +0200

    Intro about y home inicio

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -14,9 +14,11 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Información</template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    A través de nuestro email puedes contarnos tu caso. 
+    Nuestros consultores se pondrán en contacto tras estudiar tu caso para
+    poder ofrecerte un plan más especializado
+    <!--a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a--> 
+    
   </WelcomeItem>
 
   <WelcomeItem>
@@ -55,34 +57,5 @@ import SupportIcon from './icons/IconSupport.vue'
     you need more resources, we suggest paying
     <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
     a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
   </WelcomeItem>
 </template>
