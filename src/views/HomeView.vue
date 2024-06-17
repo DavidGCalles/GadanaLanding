364853fabfe745da77d1537bb65ee15aecf0e6f1
David <88436424+DavidGCalles@users.noreply.github.com>
2024-06-17 12:32:44 +0200

    KAN-8 Redistribute of texts between home and about us

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import WelcomeItem from '../components/WelcomeItem.vue'
+import DocumentationIcon from '../components/icons/IconDocumentation.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <WelcomeItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Especializados en Productos de Google</template>
+      Expertos certificados. Desde Google Workspace hasta Google Cloud,
+      nos aseguramos de que aproveche todo el potencial de estas potentes plataformas.
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Transición Digital</template>
+      Nuestro equipo de profesionales experimentados se especializa en transiciones digitales sin contratiempos. Le
+      ayudamos
+      a modernizar su
+      infraestructura y procesos para mantenerse competitivo.
+    </WelcomeItem>
+
+    <WelcomeItem>
+      <template #icon>
+        <DocumentationIcon />
+      </template>
+      <template #heading>Análisis de Datos</template>
+      Tus datos son el activo más valioso. Ofrecemos servicios avanzados de análisis de datos que transforman sus datos
+      en conocimientos accionables, mejorando el rendimiento general de su negocio.
+    </WelcomeItem>
   </main>
 </template>
